7dd0f8d558473ebf2ba243fd89b45a68bdbf6b99
Vahid Foruzanmehr <vforuzanmehr@gmail.com>
2026-04-25 08:03:36 -0400

    Add frequence and acceptable result for tuner

--- front/nava_tune/lib/models/pitch_result.dart
+++ front/nava_tune/lib/models/pitch_result.dart
@@ -1,13 +1,44 @@
 class PitchResult {
   final String note;
   final double frequency;
-  final double cents;
+  final int cents;
   final String status;
+  final double volume;
+  final int accuracy;
 
   const PitchResult({
     required this.note,
     required this.frequency,
     required this.cents,
     required this.status,
+    required this.volume,
+    required this.accuracy,
   });
+
+  PitchResult copyWith({
+    String? note,
+    double? frequency,
+    int? cents,
+    String? status,
+    double? volume,
+    int? accuracy,
+  }) {
+    return PitchResult(
+      note: note ?? this.note,
+      frequency: frequency ?? this.frequency,
+      cents: cents ?? this.cents,
+      status: status ?? this.status,
+      volume: volume ?? this.volume,
+      accuracy: accuracy ?? this.accuracy,
+    );
+  }
+
+  static const empty = PitchResult(
+    note: '--',
+    frequency: 0.0,
+    cents: 0,
+    status: 'Waiting for a note',
+    volume: 0.0,
+    accuracy: 0,
+  );
 }
